61487f39b4f3d6c3031d34acaed582b11b134e24
matveygrishaev <matveygrishaev@gmail.com>
2020-10-16 17:59:55 +0300

    save 2 leeson06

--- js/script.js
+++ js/script.js
@@ -1,2 +1,36 @@
-'use strick';
+'use strict';
 
+// Проверка на число
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n) && isFinite(n));
+};
+
+// Генератор чисел от 1 до 100;
+const getRandomIntInclusive = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Игра
+const game = function () {
+
+    let randomNumber = getRandomIntInclusive(1, 100);
+    console.log(randomNumber);
+
+    let repeat = function() {
+      let userNumber = prompt ('Угадай число от 1 до 100');
+        if (isNumber(userNumber)) {
+          if (userNumber === randomNumber) {
+            alert('Поздравляю, Вы угадали!!!');
+          } else if (userNumber > randomNumber) {
+            alert('Загаданное число меньше');
+            repeat();
+          } else if (userNumber < randomNumber) {
+            alert('Загаданное число больше');
+            repeat();
+          }
+        } else if ()
+    };
+    return repeat();
+};
+
+game();
